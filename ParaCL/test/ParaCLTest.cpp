1cc15cd49e92bc4032aea80cfca9929a7045cbaf
kefirRzevo <golubovich.ts@phystech.edu>
2024-02-21 21:43:22 +0300

    fixed test runner

--- ParaCL/test/ParaCLTest.cpp
+++ ParaCL/test/ParaCLTest.cpp
@@ -6,6 +6,8 @@
 #include <filesystem>
 #include <gtest/gtest.h>
 
+static std::filesystem::path execPath = ".";
+
 inline void readData(std::vector<int>& data, const std::string& filepath) {
     std::ifstream is{filepath, std::ios::in};
     is.seekg(0, std::ios::beg);
@@ -17,15 +19,17 @@ inline void readData(std::vector<int>& data, const std::string& filepath) {
 
 TEST(ParaclTest, RunEnd2EndTests) {
     namespace fs = std::filesystem;
-    std::string inputPath = "../tests/";
+    auto buildDir = fs::absolute(fs::path{execPath}.parent_path());
+    std::string inputPath = buildDir / std::string("../tests");
+    fs::create_directory(buildDir / std::string("../res"));
     for (auto&& p: fs::directory_iterator(inputPath)) {
-        auto testPath = fs::path(p.path());
+        auto testPath = fs::absolute(fs::path(p.path()));
         std::string testStem = testPath.stem();
         std::string testFilepath = std::string(testPath);
         if (testPath.extension().c_str() != std::string(".cl")) {
             continue;
         }
-        auto projDir = testPath.parent_path().parent_path();
+        auto projDir = fs::absolute(testPath.parent_path().parent_path());
         std::string testIn = projDir / std::string("tests") / (testStem + ".in");
         std::string testAns = projDir / std::string("tests") / (testStem + ".ans");
         std::string testOut = projDir / std::string("res") / (testStem + ".out");
@@ -37,22 +41,25 @@ TEST(ParaclTest, RunEnd2EndTests) {
         readData(outData, testOut);
         std::vector<int> ansData;
         readData(ansData, testAns);
-        auto equal = std::equal(outData.cbegin(), outData.cend(), ansData.cbegin(), ansData.cend());
+        auto equal = std::equal(outData.cbegin(), outData.cend(),
+                                ansData.cbegin(), ansData.cend());
         EXPECT_TRUE(equal);
 
-        std::string testBin =  projDir / std::string("res") / (testStem);
-        std::system(("g++ -std=c++17 -o " + testBin + " " + testCpp).c_str());
-        std::system(("./" + testBin + " < " + testIn + " > " + testCppOut).c_str());
+        std::string testCppBin =  projDir / std::string("res") / (testStem);
+        std::system(("g++ -std=c++17 -o " + testCppBin + " " + testCpp).c_str());
+        std::system((testCppBin + " < " + testIn + " > " + testCppOut).c_str());
         std::vector<int> cppData;
         readData(cppData, testCppOut);
-        auto equalCpp = std::equal(cppData.cbegin(), cppData.cend(), ansData.cbegin(), ansData.cend());
+        auto equalCpp = std::equal(cppData.cbegin(), cppData.cend(),
+                                   ansData.cbegin(), ansData.cend());
         EXPECT_TRUE(equalCpp);
     }
 }
 
-int main()
+int main(int , char* argv[])
 {
     try {
+        execPath = argv[0];
         ::testing::InitGoogleTest();
         return RUN_ALL_TESTS();
     } catch (const std::exception& e) {
